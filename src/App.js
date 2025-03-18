15e024a50fecdd720a3867b906687e56bfabf5be
potato82 <potato82@naver.com>
2025-03-18 17:36:32 +0900

    mail

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Yay~! update available!!
+          Learn React Yay~! update available!!~!
         </a>
       </header>
     </div>
